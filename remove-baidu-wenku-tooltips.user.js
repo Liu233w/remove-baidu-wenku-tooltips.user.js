bb9f7c4635ccd0f77877b449bb0c105f4cd57519
Liu233w <Liu233w@users.noreply.github.com>
2016-11-17 15:24:43 +0800

    Update remove-baidu-wenku-tooltips.user.js

--- remove-baidu-wenku-tooltips.user.js
+++ remove-baidu-wenku-tooltips.user.js
@@ -1,8 +1,9 @@
 // ==UserScript==
 // @name        移除百度文库在选中文字后弹出的提示框
 // @namespace   wwwlsmcom@outlook.com
+// @author      Liu233w
 // @include     http://wenku.baidu.com/view/*
-// @version     1
+// @version     1.0
 // @grant       none
 // @description 百度文库中在页面上选中一段文字之后会自动弹出一个提示框，相当的烦人。本脚本就是用于移除这个提示框的。
 // ==/UserScript==
